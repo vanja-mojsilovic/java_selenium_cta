5c353bf170b6388bc177473741cce3beb6c98e5f
vanja-mojsilovic <vanja.mojsilovic@spothopperapp.com>
2025-08-14 15:03:22 +0200

    messages

--- src/main/java/cta/tests/LoginTest.java
+++ src/main/java/cta/tests/LoginTest.java
@@ -14,9 +14,11 @@ public class LoginTest {
         ChromeOptions options = new ChromeOptions();
 
         if (System.getenv("CI") != null) {
-            options.addArguments("--headless");
+            options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--disable-gpu");
         }
 
         WebDriver driver = new ChromeDriver(options);
